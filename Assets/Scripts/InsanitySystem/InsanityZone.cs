db7502434970ca5f1188fdcc52bc7df4ec7a4377
Ryan <82434783+StaticYeti@users.noreply.github.com>
2022-06-11 22:19:30 -0700

    Slight change to trigger volume Insanity Zone
    Making a slight change to the on trigger enter/exit for additional testing.

--- Assets/Scripts/InsanitySystem/InsanityZone.cs
+++ Assets/Scripts/InsanitySystem/InsanityZone.cs
@@ -9,32 +9,42 @@ namespace RyanBeattie.InsanitySystem
     {
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            collision.TryGetComponent(out PlayerSystems.Player player);
+            PlayerSystems.Player player = collision.GetComponent<PlayerSystems.Player>();
 
-            if (player.playerType == PlayerSystems.PlayerType.Human)
+            if(player != null)
             {
-                Debug.Log($"I'm a Human! I am safe in the light!");
-                InsanitySystem.instance.Counting = false;
+                if (player.playerType == PlayerSystems.PlayerType.Human)
+                {
+                    Debug.Log($"I'm a Human! I am safe in the light!");
+                    InsanitySystem.instance.Counting = false;
+                }
+                if (collision.GetComponent<PlayerSystems.Player>().playerType == PlayerSystems.PlayerType.Shadow)
+                {
+                    Debug.Log($"Shadow has touched some light! You Are Dead!");
+                    //TODO Kill the shadow functionality here
+                }
             }
-            if (player.playerType == PlayerSystems.PlayerType.Shadow)
-            {
-                Debug.Log($"Shadow has touched some light! You Are Dead!");
-                //TODO Kill the shadow functionality here
-            }
+
+
         }
 
         private void OnTriggerExit2D(Collider2D collision)
         {
-            collision.TryGetComponent(out PlayerSystems.Player player);
-            if (player.playerType == PlayerSystems.PlayerType.Human)
+            PlayerSystems.Player player = collision.GetComponent<PlayerSystems.Player>();
+
+            if (player != null)
             {
-                Debug.Log($"I'm a Human! Oh No! Shadow monsters are after me, better get to the light!");
-                InsanitySystem.instance.Counting = true;
+                if (collision.GetComponent<PlayerSystems.Player>().playerType == PlayerSystems.PlayerType.Human)
+                {
+                    Debug.Log($"I'm a Human! Oh No! Shadow monsters are after me, better get to the light!");
+                    InsanitySystem.instance.Counting = true;
+                }
+                if (collision.GetComponent<PlayerSystems.Player>().playerType == PlayerSystems.PlayerType.Shadow)
+                {
+                    Debug.Log($"I'm a Shadow! I need to stay in the darkness!");
+                }
             }
-            if(player.playerType == PlayerSystems.PlayerType.Shadow)
-            {
-                Debug.Log($"I'm a Shadow! I need to stay in the darkness!");
-            }
+
         }
     }
 
