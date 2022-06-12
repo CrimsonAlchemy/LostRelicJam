41f68f398d4a528504b7b02ee7ea7036f219acfc
DhruvS28 <28dhruv03@gmail.com>
2022-06-12 15:06:14 -0400

    Camera Follow added

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    public float speed;
+
+    // public Vector3 offset;
+
+    void Start()
+    {
+        speed = 0.125f;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, -1f); 
+    }
+}
+
