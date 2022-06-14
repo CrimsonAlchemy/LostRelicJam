63cfa68e3490a12f5ad433d5e34a3ab5e55696cb
DhruvS28 <28dhruv03@gmail.com>
2022-06-14 10:38:42 -0400

    Automatic lighting script per tile on tilemap

---
+++ Assets/Scripts/TileSystem/TileFinder.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+namespace DhruvS28.TileSystem
+{
+    public class TileFinder : MonoBehaviour
+    {
+        
+        public List<Vector3> tilePositions;
+ 
+        // Start is called before the first frame update
+        public List<Vector3> GetTiles()
+        {
+            Tilemap tilemap = GetComponent<Tilemap>();
+
+            foreach (var position in tilemap.cellBounds.allPositionsWithin)
+            {
+                if (!tilemap.HasTile(position))
+                {
+                    continue;
+                }
+
+                // Tile is not empty; do stuff.
+                tilePositions.Add(position);
+            }
+
+            return tilePositions;
+        }
+    }
+}
---
+++ Assets/Scripts/TileSystem/TileLighting.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.Rendering.Universal;
+
+
+namespace DhruvS28.TileSystem
+{
+    public class TileLighting : MonoBehaviour
+    {
+
+        private List<Vector3> tilePositions;
+
+        public GameObject torches;
+        public GameObject lightPrefab;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Tilemap tilemap = GetComponent<Tilemap>();
+
+            foreach (var position in tilemap.cellBounds.allPositionsWithin)
+            {
+                if (!tilemap.HasTile(position))
+                {
+                    continue;
+                }
+
+                // Tile is not empty; do stuff.
+                tilePositions.Add(position);
+            }
+
+
+            foreach (var tileP in tilePositions)
+            {
+                GameObject light = Instantiate(lightPrefab, tileP + new Vector3(0.5f, 0.5f, 0f), Quaternion.identity);
+
+                light.transform.SetParent(torches.transform);
+                //light.gameObject.AddComponent<Light2D>();
+            }
+        }
+    }
+}
