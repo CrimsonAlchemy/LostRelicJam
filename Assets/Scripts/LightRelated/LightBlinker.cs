2c414df79f3ae92dbe271ca4d9fd4f97262839ec
DhruvS28 <28dhruv03@gmail.com>
2022-06-17 01:56:09 -0400

    Dark Zone?

--- Assets/Scripts/LightRelated/LightBlinker.cs
+++ Assets/Scripts/LightRelated/LightBlinker.cs
@@ -9,44 +9,55 @@ namespace DhruvS28.LightSystem
     public class LightBlinker : MonoBehaviour
     {
 
-        Transform mainLight;
-        Transform flickerLight;
-        Light2D mainLightComponent;
-        Light2D flickerLightComponent;
+        Transform blinkerLight;
+        Light2D blinkerLightComponent;
 
 
         // Start is called before the first frame update
         void Start()
         {
-            mainLight = this.transform;
-            flickerLight = this.transform.GetChild(0);
-            mainLightComponent = mainLight.GetComponent<Light2D>();
-            flickerLightComponent = flickerLight.GetComponent<Light2D>();
+            blinkerLight = this.transform;
+            //blinkerLight = this.transform.GetChild(0);
+            //mainLightComponent = mainLight.GetComponent<Light2D>();
+            blinkerLightComponent = blinkerLight.GetComponent<Light2D>();
 
-            StartCoroutine(Flicker());
+            StartCoroutine(Blinker());
         }
 
-        IEnumerator Flicker()
+        IEnumerator Blinker()
         {
-            float flikcerTime = Random.Range(10f, 20f);
+            //float flikcerTime = Random.Range(10f, 20f);
+            float blinkerTime = 15f;
+            float change = -0.1f;
 
-            for (int i = 0; i < flikcerTime; i++) //this is while(true)
+            //for (float i = 0; i < blinkerTime; i++) //this is while(true)
+            for( ; ; )
             {
-                float randomIntensity = Random.Range(0.5f, 1f);
-                flickerLightComponent.intensity = randomIntensity;
+                if (blinkerLightComponent.intensity <= 0)
+                    change = 0.1f;
+                else if (blinkerLightComponent.intensity >= 1.5)
+                {
+                    yield return new WaitForSeconds(1f);
+                    change = -0.1f;
+                }
+                //float randomIntensity = Random.Range(0.5f, 1f);
+                //flickerLightComponent.intensity = randomIntensity;
 
-                float randomTime = Random.Range(0.05f, 0.15f);
-                yield return new WaitForSeconds(randomTime);
+                //float randomTime = Random.Range(0.05f, 0.15f);
+                //Debug.Log("Blinking: " + i);
+                blinkerLightComponent.intensity += change;
+
+                yield return new WaitForSeconds(0.01f);
             }
-            flickerLightComponent.intensity = 0.5f;
-            yield return StartCoroutine(FlickerDelay());
+            //yield return new WaitForSeconds(1f);
+            //yield return StartCoroutine(Blinker());
         }
 
-        IEnumerator FlickerDelay()
-        {
-            float randomTime = Random.Range(0.5f, 1.2f);
-            yield return new WaitForSeconds(randomTime);
-            yield return StartCoroutine(Flicker());
-        }
+        //IEnumerator FlickerDelay()
+        //{
+        //    float randomTime = Random.Range(0.5f, 1.2f);
+        //    yield return new WaitForSeconds(randomTime);
+        //    yield return StartCoroutine(Flicker());
+        //}
     }
 }
