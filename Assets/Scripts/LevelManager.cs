26784d6b904a920d518cc15172174abe7ca6bb85
DhruvS28 <28dhruv03@gmail.com>
2022-06-18 18:05:02 -0400

    Fading Effect

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,9 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+
+    public bool ReloadLevel;
+    public Animator anim;
+
+    private void Start()
+    {
+        anim.Play("FadeIn");
+    }
+
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    StartCoroutine(SwitchScene(SceneManager.GetActiveScene().ToString()));
+        //}
+    }
+
     public void LoadNextLevel(string levelToLoad)
     {
-        SceneManager.LoadScene(levelToLoad);
+        if(ReloadLevel)
+            SwitchScene(SceneManager.GetActiveScene().ToString());
+        else
+            SceneManager.LoadScene(levelToLoad);
+    }
+
+
+    public IEnumerator SwitchScene(string newScene)
+    {
+        anim.Play("FadeOut");
+        yield return new WaitForSeconds(1f);
+
+        LoadNextLevel(newScene);
     }
 
 }
